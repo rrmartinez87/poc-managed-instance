b36605a37383c3045e0bf68b0b61fc1af1b908f8
rrmartinez87 <rafael.martinez.angel@gmail.com>
2020-06-17 09:05:18 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,7 +47,7 @@ variable "subnet_name" {
 variable "subnet_address_prefixes" {
     description = "The address prefixes to use for the subnet."
     type = list(string)
-    default     = ["10.0.1.0/24"]
+    default     = ["10.0.0.0/24"]
 }
 
 variable "delegation_name" {
